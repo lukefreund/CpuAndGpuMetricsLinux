edca59e10685f29fa5df15ffc3ea61561c8a3d3f
B-S200502 <basemsousou02@gmail.com>
2023-11-06 14:04:12 -0500

    Initial
    6/11/2023
    -Write to excel added

--- Encode_Decode_Testing_Automation/Encode_Decode_Testing_Automation/Program.cs
+++ Encode_Decode_Testing_Automation/Encode_Decode_Testing_Automation/Program.cs
@@ -3,6 +3,7 @@ using static CpuAndGpuMetrics.GpuMetricRetriever;
 using CpuAndGpuMetrics;
 using System.Diagnostics;
 using System.IO;
+using OfficeOpenXml; //main namespace for EPPlus library (allows us to manipulate the excel file)
 
 class Program
 {
@@ -28,6 +29,37 @@ class Program
 
         container.PopulateData();
 
+        float cpuMetrics = container.CpuUsage;
+
+        float gpuMetrics = container.GpuOverall;
+
+        string file_path = @"C:\Users\tester\.....\CPUandGPUDecode.xlsx"; //TODO: NEED TO PUT CORRECT PATH AND MAKE A FILE THERE
         
+        WriteToExcel(file_path, cpuMetrics, gpuMetrics);
+
+        Console.WriteLine("Data successfully written to Excel.");
+    }
+    
+    static void WriteToExcel(string file_path, float cpuMetrics, float gpuMetrics)
+    {
+        using(ExcelPackage CPUandGPU_Decode = new ExcelPackage())
+        {
+            ExcelWorksheet Raw_Data1 = CPUandGPU_Decode.Workbook.Worksheets.Add("Utilization Data"); //making new worksheet and renaming it
+
+            Raw_Data1.Cells[1, 1].Value = "Cpu Utilization";
+            Raw_Data1.Cells[1, 2].Value = "Gpu Utilization";
+            Raw_Data1.Cells[1, 3].Value = "3D";
+
+            Raw_Data1.Cells[2, 1].Value = cpuMetrics;
+            Raw_Data1.Cells[2, 1].Value = gpuMetrics;
+            Raw_Data1.Cells[2, 1].Value = gpuMetrics;
+
+
+            //Converting entire ExcelPackage to a byte array (prep for writing data to excel)
+            byte[] draft1 = CPUandGPU_Decode.GetAsByteArray();
+
+            //Writes the array to a file at the specified "path"
+            File.WriteAllBytes(file_path, draft1);
+        }
     }
 }
