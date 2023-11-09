e9865ae8c6edd6cec4f215adf99bf8dfcb23c860
B-S200502 <basemsousou02@gmail.com>
2023-11-09 14:41:47 -0500

    Excel File Formatting
    11/09/2023
    - greyed out headers
    - colored cells depeding on hwaccel, chroma, and codec
    - automatically generates xlsx file in desired path
    - working on adding filters

--- Encode_Decode_Testing_Automation/Encode_Decode_Testing_Automation/Program.cs
+++ Encode_Decode_Testing_Automation/Encode_Decode_Testing_Automation/Program.cs
@@ -7,11 +7,19 @@ using static CpuAndGpuMetrics.Video;
 using System;
 using System.Runtime.CompilerServices;
 using System.Collections;
+using System.Drawing;
 
 class Program
 {
     readonly static string TESTSOURCESPATH = @"..\..\..\OfficialSources";
-    readonly static string EXCELOUTPATH = @"C:\Users\bsousou\Documents\GitHub\CPUAndGPUMetrics\test1.xlsx";
+
+    //SPECIFY PATH WHERE YOU WOULD LIKE THE EXCEL FILES TO BE DUMPED
+    readonly static string EXCELDIRECTORY = @"C:\Users\bsousou\Documents\GitHub\CPUAndGPUMetrics\";
+    static int testno = 0;
+
+    //Combining file name and path
+    static string fileName = $"AutomatedData_#{++testno}.xlsx";
+    static string filePath = Path.Combine(EXCELDIRECTORY, fileName);
 
     static void Main()
     {
@@ -51,12 +59,11 @@ class Program
                 //P?.WaitForExit();
             }
             // DEBUG
-            string file_path = EXCELOUTPATH; //TODO: NEED TO PUT CORRECT PATH AND MAKE A FILE THERE
-            DictToExcel(videoPerfData, file_path);
+            //string file_path = EXCELDIRECTORY; //TODO: NEED TO PUT CORRECT PATH AND MAKE A FILE THERE
+            //DictToExcel(videoPerfData, file_path);
+
+            DictToExcel(videoPerfData, filePath);
         }
-
-
-
     }
 
     static void DictToExcel
@@ -64,9 +71,12 @@ class Program
         string file_path)
     {
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-        using ExcelPackage CPUandGPU_Decode = new();
 
-        ExcelWorksheet Raw_Data1 = CPUandGPU_Decode.Workbook.Worksheets.Add("Automated Utilization Data");
+        //Creating new excel package named CPUandGPU_Decode
+        using ExcelPackage CPUandGPU_Decode = new ExcelPackage();
+
+        //adding a new worksheet
+        ExcelWorksheet worksheet = CPUandGPU_Decode.Workbook.Worksheets.Add("Automated Utilization Data");
 
         //Creating the Headers
         string[] headers = new string[]
@@ -77,23 +87,28 @@ class Program
         };
 
         //Resizing the columns
-        Raw_Data1.Column(1).Width = 5;
+        worksheet.Column(1).Width = 5;
         for (int col = 2; col < headers.Length + 1; col++)
         {
-            Raw_Data1.Column(col).Width = 15;
+            worksheet.Column(col).Width = 18;
         }
 
-        if (Raw_Data1.Dimension == null)
+        //Formatting the Excel sheet
+        if (worksheet.Dimension == null)
         {
             for (int i = 0; i < headers.Length; i++)
             {
-                Raw_Data1.Cells[1, i + 1].Value = headers[i];
+                worksheet.Cells[1, i + 1].Value = headers[i];
+                //Raw_Data1.Cells[1, i + 1].IsRichText = true;
+
 
                 //Centering the headers
-                Raw_Data1.Cells[1, i + 1].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                worksheet.Cells[1, i + 1].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
 
-                //Creating filters for each header TODO: Fix this its only doing it for the last column (using Auto Filter)
-                Raw_Data1.Cells[1, i + 1].AutoFilter = true;
+                //Changing color of headers row to grey using EPPlus
+                worksheet.Cells[1, i + 1].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                worksheet.Cells[1, i + 1].Style.Fill.BackgroundColor.SetColor(Color.LightGray);
+
             }
         }
 
@@ -106,21 +121,25 @@ class Program
             container = tupleEntry.Item2;
             hwaccel = tupleEntry.Item3;
 
-            WriteToExcel(Raw_Data1, video, container, hwaccel.HardwareAccel, hwaccel.Gpu, testCounts++);
+            WriteToExcel(worksheet, video, container, hwaccel.HardwareAccel, hwaccel.Gpu, testCounts++);
         }
 
 
-        //Converting entire ExcelPackage to a byte array (prep for writing data to excel)
-        byte[] draft1 = CPUandGPU_Decode.GetAsByteArray();
+        ////Converting entire ExcelPackage to a byte array (prep for writing data to excel)
+        //byte[] draft1 = CPUandGPU_Decode.GetAsByteArray();
 
-        //Writes the array to a file at the specified path
-        File.WriteAllBytes(file_path, draft1);
+        ////Writes the array to a file at the specified path
+        //File.WriteAllBytes(file_path, draft1);
+
+        byte[] fileBytes = CPUandGPU_Decode.GetAsByteArray();
+        File.WriteAllBytes(file_path, fileBytes);
+
 
         Console.WriteLine("Data successfully written to Excel.");
     }
 
     static void WriteToExcel
-        (ExcelWorksheet Raw_Data1, 
+        (ExcelWorksheet worksheet, 
         Video video, PerformanceMetricsContainer container, string hardwareAccel,  GpuType? gpu, int testCounts)
     {
         //Can add if-else statements to change the color of the cells depending on chroma and color format
@@ -148,22 +167,81 @@ class Program
         string hwaccel = hardwareAccel;
         int newRow = testCounts + 1;
 
-        Raw_Data1.Cells[newRow, 1].Value = testCounts;
-        Raw_Data1.Cells[newRow, 2].Value = OS;
-        Raw_Data1.Cells[newRow, 3].Value = gpuType;
-        Raw_Data1.Cells[newRow, 4].Value = decodeMethod;
-        Raw_Data1.Cells[newRow, 5].Value = hwaccel;
-        Raw_Data1.Cells[newRow, 6].Value = codec;
-        Raw_Data1.Cells[newRow, 7].Value = chroma;
-        Raw_Data1.Cells[newRow, 8].Value = bitDepth;
-        Raw_Data1.Cells[newRow, 9].Value = resolution;
+        //Organizing the Column headers into their correct positions
+        worksheet.Cells[newRow, 1].Value = testCounts;
+        worksheet.Cells[newRow, 2].Value = OS;
+        worksheet.Cells[newRow, 3].Value = gpuType;
+        worksheet.Cells[newRow, 4].Value = decodeMethod;
+        worksheet.Cells[newRow, 5].Value = hwaccel;
+        worksheet.Cells[newRow, 6].Value = codec;
+        worksheet.Cells[newRow, 7].Value = chroma;
+        worksheet.Cells[newRow, 8].Value = bitDepth;
+        worksheet.Cells[newRow, 9].Value = resolution;
 
-        Raw_Data1.Cells[newRow, 10].Value = finalFPS;
-        Raw_Data1.Cells[newRow, 11].Value = cpuUsage;
-        Raw_Data1.Cells[newRow, 12].Value = gpuUsage;
-        Raw_Data1.Cells[newRow, 13].Value = gpu3d;
-        Raw_Data1.Cells[newRow, 14].Value = vidDec0;
-        Raw_Data1.Cells[newRow, 15].Value = vidDec1;
-        Raw_Data1.Cells[newRow, 16].Value = vidDec2;
+        worksheet.Cells[newRow, 10].Value = finalFPS;
+        worksheet.Cells[newRow, 11].Value = cpuUsage;
+        worksheet.Cells[newRow, 12].Value = gpuUsage;
+        worksheet.Cells[newRow, 13].Value = gpu3d;
+        worksheet.Cells[newRow, 14].Value = vidDec0;
+        worksheet.Cells[newRow, 15].Value = vidDec1;
+        worksheet.Cells[newRow, 16].Value = vidDec2;
+
+
+        //More formatting: Coloring the hwaccel, codec, and chroma for further organization
+        if (hwaccel == "cuda")
+        {
+            worksheet.Cells[newRow, 5].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 5].Style.Fill.BackgroundColor.SetColor(Color.LightSlateGray);
+        }
+        else if (hwaccel == "d3d11va")
+        {
+            worksheet.Cells[newRow, 5].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 5].Style.Fill.BackgroundColor.SetColor(Color.LightSteelBlue);
+        }
+        else if (hwaccel == "vulkan")
+        {
+            worksheet.Cells[newRow, 5].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 5].Style.Fill.BackgroundColor.SetColor(Color.LightYellow);
+        }
+        else if (hwaccel == "vaapi")
+        {
+            worksheet.Cells[newRow, 5].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 5].Style.Fill.BackgroundColor.SetColor(Color.LightSalmon);
+        }
+        else if (hwaccel == "none")
+        {
+            worksheet.Cells[newRow, 5].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 5].Style.Fill.BackgroundColor.SetColor(Color.Yellow);
+        }
+        else if (hwaccel == "qsv")
+        {
+            worksheet.Cells[newRow, 5].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 5].Style.Fill.BackgroundColor.SetColor(Color.LightSkyBlue);
+        }
+        
+
+        if (codec == "h264" || codec == "H264")
+        {
+            worksheet.Cells[newRow, 6].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 6].Style.Fill.BackgroundColor.SetColor(Color.Salmon);
+        }
+        else if (codec == "h265" || codec == "H265")
+        {
+            worksheet.Cells[newRow, 6].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 6].Style.Fill.BackgroundColor.SetColor(Color.LightGreen);
+        }
+
+
+        if (chroma == "Subsampling_420")
+        {
+            worksheet.Cells[newRow, 7].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 7].Style.Fill.BackgroundColor.SetColor(Color.Green);
+        }
+        else if (chroma == "Subsampling_444")
+        {
+            worksheet.Cells[newRow, 7].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            worksheet.Cells[newRow, 7].Style.Fill.BackgroundColor.SetColor(Color.Red);
+        }
+
     }
 }
